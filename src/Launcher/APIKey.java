03e6aa5e39b35165f1460e9fcdef5d6013b185ee
ryan-doan <82603138+ryan-doan@users.noreply.github.com>
2021-12-27 09:49:31 -0600

    Delete APIKey.java

--- src/Launcher/APIKey.java
+++
@@ -1,9 +0,0 @@
-package Launcher;
-
-public class APIKey {
-    static String APIKey = "api_key=RGAPI-338d8333-8926-4667-8c43-bf78b2ee3e47";
-
-    public static String getAPIKey() {
-        return APIKey;
-    }
-}
